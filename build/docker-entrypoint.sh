5ac60fd7bfa266dd907e668a236c8ca9729520f6
Zakhi Verech <zakhi@omnisol.co.il>
2017-04-18 11:07:03 +0300

    Make researcher ID a link

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+cp -r $VIVO_ORIG_HOME/* $VIVO_HOME
+
 if [ ! -f "$VIVO_HOME/runtime.properties" ]; then
-    cp -r $VIVO_ORIG_HOME/* $VIVO_HOME
     cp $VIVO_HOME/config/runtime.properties $VIVO_HOME/runtime.properties
     cp $VIVO_HOME/config/example.applicationSetup.n3 $VIVO_HOME/config/applicationSetup.n3
 fi
