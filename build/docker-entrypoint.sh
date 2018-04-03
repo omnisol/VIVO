583d88fc2409bec146da225e9dcd03761023128e
Zakhi Verech <zakhi@omnisol.co.il>
2018-04-03 09:44:08 +0300

    Fix docker entrypoint

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -4,6 +4,9 @@ cp -r $VIVO_ORIG_HOME/* $VIVO_HOME
 
 if [ ! -f "$VIVO_HOME/runtime.properties" ]; then
     cp $VIVO_HOME/config/example.runtime.properties $VIVO_HOME/runtime.properties
+fi
+
+if [ ! -f "$VIVO_HOME/config/applicationSetup.n3" ]; then
     cp $VIVO_HOME/config/example.applicationSetup.n3 $VIVO_HOME/config/applicationSetup.n3
 fi
 
